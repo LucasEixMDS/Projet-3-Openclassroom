9f485882da9a84620d769febc9714a00a49945b0
Lucas Eix <lucas.melodiedessens@gmail.com>
2023-05-08 09:28:48 +0200

    enregistrement

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,20 +1,25 @@
-const button = document.getElementById("connexion")
-const form = document.querySelector("#login-form")
+// Récupération du bouton de connexion et du formulaire de connexion
+const button = document.getElementById("connexion");
+const form = document.querySelector("#login-form");
 
-
+// Ajout d'un écouteur d'événements sur le formulaire lorsque l'utilisateur clique sur le bouton de connexion
 form.addEventListener("submit", function (event) {
-  event.preventDefault();
+  event.preventDefault(); // Empêche le formulaire de se soumettre à la page suivante qui recharge la page
 
-  const error = document.querySelector('#error-message')
+  // Récupération de l'élément d'affichage des erreurs
+  const error = document.querySelector('#error-message');
 
+  // Récupération des valeurs saisies par l'utilisateur dans les champs email et mot de passe
   const emailUser = document.querySelector("#email").value;
   const password = document.querySelector("#password").value;
   
+  // Création d'un objet user contenant l'email et le mot de passe saisis par l'utilisateur
   const user = {
       email: emailUser, 
       password : password, 
   }
 
+  // Envoi des informations de connexion à l'API serveur en utilisant la méthode POST
   fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     headers: {
@@ -24,23 +29,26 @@ form.addEventListener("submit", function (event) {
   })
   .then(response => {
     if (response.ok) {
+      // Si la réponse est positive, le serveur renvoie un objet JSON contenant l'identifiant de l'utilisateur et un token d'authentification
+      // Extraction de ces données depuis la réponse JSON
       return response.json()
         .then(data => {
           const { userId, token } = data;
-            // Stocker l'ID utilisateur et le jeton d'authentification localement
-        sessionStorage.setItem("userId", userId);
-        sessionStorage.setItem("token", token);
-        // Rediriger l'utilisateur vers la page suivante
-        window.location.replace("index.html");
-      })
-        
+          // Stockage de l'identifiant utilisateur et du token d'authentification localement sur le navigateur
+          sessionStorage.setItem("userId", userId);
+          sessionStorage.setItem("token", token);
+          // Redirection de l'utilisateur vers la page suivante
+          window.location.replace("index.html");
+      })  
     } else if (response.status === 404){
+      // Si la réponse indique une erreur 404, l'identifiant de l'utilisateur est incorrect
       error.innerHTML = "erreur dans l'identifiant" ;
     } else if (response.status === 401){
+      // Si la réponse indique une erreur 401, le mot de passe est incorrect
       error.innerHTML = "erreur mot de passe" ;
     } 
   })
-  });
+});
 
 
 
--- FrontEnd/logout.js
+++ FrontEnd/logout.js
@@ -1,12 +1,14 @@
+
+// function de deconnexion 
 function logout(){
 
     const buttonLogout = document.querySelector(".btn-logout");
 
     buttonLogout.addEventListener("click", function () {
-    // Supprimer le token et l'ID utilisateur du localStorage
+    // Supprimer le token et utilisateur du sessionStorage
     sessionStorage.removeItem("userId");
     sessionStorage.removeItem("token");
-    // Rediriger l'utilisateur vers la page de connexion
+    // Rediriger l'utilisateur vers la page html 
     window.location.replace("./login.html");
     });
 }
